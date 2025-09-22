c8b00b6a0d1ed47ea051dbe94bc6bc208f4e1865
Guilherme Macedo <souzaarthurgui@gmail.com>
2025-09-22 01:15:11 -0300

    Corrige URL do servidor

--- painel.js
+++ painel.js
@@ -4,7 +4,7 @@ document.addEventListener('DOMContentLoaded', () => {
     const readyOrdersList = document.getElementById('ready-orders');
     const noOrdersMessage = document.getElementById('no-orders-message');
 
-    const serverURL = 'https://cardapio-digital-teal.vercel.app/';
+    const serverURL = 'https://cardapio-digital-teal.vercel.app';
 
     async function fetchOrders() {
         try {
